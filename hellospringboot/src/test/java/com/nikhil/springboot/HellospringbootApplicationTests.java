543654a55f6d2eaf197d0b9ba8ea7ae747392b29
nikhil-kumar6 <97690669+nikhil-kumar6@users.noreply.github.com>
2023-01-25 18:05:15 +0530

    Handson on collection framework

---
+++ hellospringboot/src/test/java/com/nikhil/springboot/HellospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhil.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
